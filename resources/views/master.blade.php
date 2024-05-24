0b96164fd1a4b1ab2d8e9dd4223a5ad430ef307c
Nick Mathew <nickmathew331@gmail.com>
2024-05-24 22:30:00 +0700

    make template for navbar

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,7 +3,11 @@
 
 <head>
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('styles')
+    <link rel="icon" href="{{ asset('images/MangaMis.ico') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{asset('Style/bootstrap.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
@@ -20,6 +24,10 @@
     </footer>
 
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+
 </body>
 
 </html>
