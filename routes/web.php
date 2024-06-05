4c62a05b2f12d243e0252054752cacae0eb21acc
ShinmenTengen <andre667341@gmail.com>
2024-06-05 21:21:24 +0700

    API updated manga controller and services

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UpdatedMangaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,3 +33,7 @@ Route::get('/navbar', function(){
 Route::get('/reg', function(){
     return view('try_register');
 });
+
+Route::get('/updated-manga', [
+    UpdatedMangaController::class, 'showUpdatedManga'
+]);
